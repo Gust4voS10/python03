192b7c9c4a5f3ca9cb22d63ad1a4489f22edc262
gustada- <gustada-@student.42.rio>
2026-04-10 20:58:09 -0300

    ft_inventory_system

---
+++ ex3/ft_achievement_tracker.py
@@ -0,0 +1,36 @@
+import random
+
+def main():
+    achievements = {"Crafting Genius", "Strategist", "World Savior", "Speed Runner", "Survivor",
+                    "Master Explorer", "Treasure Hunter", "Unstoppable", "First Steps", "Collector Supreme",
+                    "Untouchable", "Sharp Mind", "Boss Slayer"}
+    Alice: set = set()
+    Bob: set = set()
+    Charlie: set = set()
+    Dylan: set = set()
+    for _ in range(7):
+        Alice.add(random.choice(list(achievements)))
+        Bob.add(random.choice(list(achievements)))
+        Charlie.add(random.choice(list(achievements)))
+        Dylan.add(random.choice(list(achievements)))
+    print(f"Player Alice: {Alice}")
+    print(f"Player Bob: {Bob}")
+    print(f"Player Charlie: {Charlie}")
+    print(f"Player Dylan: {Dylan}")
+    All_distinct = Alice.union(Bob).union(Charlie).union(Dylan)
+    print(f"\nAll distinct achievements: {All_distinct}")
+    common_achievements = Alice.intersection(Bob).intersection(Charlie).intersection(Dylan)
+    print(f"\nCommon achievements: {common_achievements}\n")
+    print(f"Only Alice: {Alice.difference(Bob).difference(Charlie).difference(Dylan)}")
+    print(f"Only Bob: {Bob.difference(Alice).difference(Charlie).difference(Dylan)}")
+    print(f"Only Charlie: {Charlie.difference(Alice).difference(Bob).difference(Dylan)}")
+    print(f"Only Dylan: {Dylan.difference(Alice).difference(Bob).difference(Charlie)}")
+    print(f"\nAlice is missing: {achievements.difference(Alice)}")
+    print(f"Bob is missing: {achievements.difference(Bob)}")
+    print(f"Charlie is missing: {achievements.difference(Charlie)}")
+    print(f"Dylan is missing: {achievements.difference(Dylan)}")
+
+
+if __name__ == "__main__":
+    print("=== Achievement Tracker System ===\n")
+    main()
